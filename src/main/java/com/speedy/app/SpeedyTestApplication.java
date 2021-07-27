6482426ed93693eb2ccf5938c55b6166ebde1559
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2021-07-27 20:45:13 -0300

    :sparkles: : Project setup

---
+++ src/main/java/com/speedy/app/SpeedyTestApplication.java
@@ -0,0 +1,13 @@
+package com.speedy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedyTestApplication.class, args);
+	}
+
+}
